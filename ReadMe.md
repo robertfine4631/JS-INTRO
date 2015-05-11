a50f4e20c09eab22cd1b7ecedf8b23493bd577cc
Robert Fine <robertfine4631@icloud.com>
2015-05-11 13:40:50 -0400

    Initial Commit

---
+++ ReadMe.md
@@ -0,0 +1,3 @@
+#This is my homework.
+ It is a basic introduction to javascript. I added an "if" statement just for fun
+ There are no errors when processed through the javascript console.
---
+++ main.js
@@ -0,0 +1,68 @@
+var givenName;
+// Q: What does `givenName` equal right now?
+
+// Undefined
+console.log (givenName);
+givenName = "Tim";
+// Q: What is `givenName` set to right now?
+
+// "Tim"
+console.log (givenName);
+givenName = "JD";
+// Q: What is `givenName` set to right now?
+
+// "JD"
+console.log (givenName);
+
+
+var greeting = "Hello, how are you?";
+// Q: What is `greeting` set to?
+
+// "Hello, how are you?"
+console.log (greeting);
+
+
+var high = 50;
+var low  = 10;
+
+
+var math = high - low;
+// Q: What is `math` set to?
+
+// 40
+console.log (math);
+
+math = high - "5";
+// Q: What is `math` set to?
+
+// 45
+
+// Create a variable to calculate Tim's age
+// The answer should read "Tim is 31 years old"
+var born = 1983;
+var today = 2015;
+
+// Answer 
+
+var tAge = today - born;
+	if (tAge = 31) {
+		console.log	("Tim is 31 years old");
+};
+
+console.log (tAge);
+
+
+// Store some information following in variables.
+var yourName;
+var instructorName;
+
+// Answer
+yourName = "Bobby"
+instructorName = "Tim"
+
+
+var statement = yourName + " is taking a class at The Iron Yard, my instructor's name is " + instructorName;
+
+//Answer 2
+
+console.log (statement);
